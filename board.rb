96cd665864f15853a14f6e8c2d64321dc7a31c87
alaughlin <adamlaughlin@outlook.com>
2014-07-17 08:21:56 -0400

    Wrote generate_grid and make_rows methods for Board class.

--- board.rb
+++ board.rb
@@ -1,2 +1,39 @@
+require './piece.rb'
+
 class Board
+  def initialize(grid = nil)
+    @grid = grid.nil? ? generate_grid : grid
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos, obj)
+    x, y = pos
+    @grid[x][y] = obj
+  end
+
+  private
+  def generate_grid
+    grid = Array.new(8) { Array.new { nil } }
+
+    make_rows(grid, fst, lst, :red)
+    make_rows(grid, fst, lst)
+    make_rows(grid, fst, lst, :white)
+
+    grid
+  end
+
+  def make_rows(grid, fst, lst, color = nil)
+    fst.upto(lst) do |row|
+      0.upto(y) do |col|
+        if ((row.even? && col.odd?) || (row.odd? && col.even?)) && !color.nil?
+          grid[row, col] = Piece.new(color, [row, col], grid)
+        else
+          grid[row, col] = nil
+      end
+    end
+  end
 end
