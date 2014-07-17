6b64a6b9c59bfd295debc77b1b96c82bc6c2cffd
alaughlin <adamlaughlin@outlook.com>
2014-07-17 09:41:00 -0400

    Start work on jump logic.

--- piece.rb
+++ piece.rb
@@ -1,7 +1,55 @@
+class InvalidMoveError < ArgumentError
+end
+
 class Piece
   def initialize(color, position, grid, kinged = false)
-    puts "making piece!"
     @color, @position, @grid, @kinged = color, position, grid, kinged
+  end
+
+  def move(target)
+    slide_offsets = slide_diffs(@color)
+    jump_offsets = jump_diffs(@color)
+    current_offset = [target[0] - @position[0], target[1] - @position[1]]
+
+    if jump_offsets.include?(current_offset)
+      perform_jump(target)
+    else slide_offsets.include?(current_offset)
+      perform_slide(target)
+    end
+
+    nil
+  end
+
+  def perform_slide(target)
+    if @grid[target].nil?
+      @grid[@position] = nil
+      @position = target
+      @grid[@position] = self
+    else
+      raise InvalidMoveError
+    end
+  end
+
+  def perform_jump(target)
+    if @grid[target].nil?
+      @grid[@position] = nil
+      @position = target
+      @grid[@position] = self
+    else
+      raise InvalidMoveError
+    end
+  end
+
+  def slide_diffs(color)
+    color == :red ? [[1, -1], [1, 1]] : [[-1, -1], [-1, 1]]
+  end
+
+  def jump_diffs(color)
+    color == :red ? [[2, -2], [2, 2]] : [[-2, -2], [-2, 2]]
+  end
+
+  def maybe_promote
+
   end
 
   def render
